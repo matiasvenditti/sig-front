23a20ab4b8355fefba68ada1c00bc95e115c9be5
Matias <matias.venditti@ing.austral.edu.ar>
2019-10-30 09:43:54 -0300

    create orders

--- src/app/order/create-order/create-order.component.ts
+++ src/app/order/create-order/create-order.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductService } from 'src/app/services/product/product.service';
+import { NbDialogRef } from '@nebular/theme';
+import { ToasterService } from 'src/app/services/toaster.service';
+import { ProductDTO } from 'src/app/dto/procuct-dto';
+import { OrderService } from 'src/app/services/order/order.service';
 
 @Component({
   selector: 'app-create-order',
@@ -7,9 +13,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrderComponent implements OnInit {
 
-  constructor() { }
+  private orderForm: FormGroup;
+  private products: ProductDTO[] = [];
+
+  constructor(private fb: FormBuilder,
+    private productService: ProductService,
+    private dialogRef: NbDialogRef<CreateOrderComponent>,
+    private toasterService: ToasterService,
+    private orderService: OrderService) { }
 
   ngOnInit() {
+    this.orderForm = this.fb.group({
+      amount: [null, [Validators.required, Validators.min(0)]],
+      price: [null, [Validators.required, Validators.min(0)]],
+      product: [null, Validators.required]
+    });
+
+    this.productService.getAll().subscribe(res => {
+      this.products = res;
+    })
+  }
+
+  submit() {
+    this.orderService.create(this.orderForm.value).subscribe(res => {
+      this.toasterService.showSuccess('Orden generada exitosamente','Operación Exitosa');
+      this.dialogRef.close(res);
+    }, () => {
+      this.toasterService.showError('No se pudo generar la orden correctamente', 'Error');
+      this.close();
+    });
+  }
+
+  close() {
+    this.dialogRef.close();
   }
 
 }
