32fe6fc1923457598e52e988e362d8e4dbcf9274
Matias <matias.venditti@ing.austral.edu.ar>
2019-10-30 10:07:25 -0300

    update order

--- src/app/order/update-order/update-order.component.ts
+++ src/app/order/update-order/update-order.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ProductService } from 'src/app/services/product/product.service';
+import { NbDialogRef } from '@nebular/theme';
+import { ToasterService } from 'src/app/services/toaster.service';
+import { ProductDTO } from 'src/app/dto/procuct-dto';
+import { OrderService } from 'src/app/services/order/order.service';
+import { OrderDTO } from 'src/app/dto/order-dto';
 
 @Component({
   selector: 'app-update-order',
@@ -7,9 +14,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateOrderComponent implements OnInit {
 
-  constructor() { }
+  private orderForm: FormGroup;
+  private products: ProductDTO[] = [];
+  private min: Date;
+  private order: OrderDTO;
+
+  constructor(private fb: FormBuilder,
+    private productService: ProductService,
+    private dialogRef: NbDialogRef<UpdateOrderComponent>,
+    private toasterService: ToasterService,
+    private orderService: OrderService) { }
 
   ngOnInit() {
+    this.min = new Date();
+    
+    this.orderForm = this.fb.group({
+      amount: [this.order.amount, [Validators.required, Validators.min(0)]],
+      price: [this.order.price, [Validators.required, Validators.min(0)]],
+      product: [this.order.product, Validators.required],
+      createdDate: [null, Validators.required]
+    });
+
+    this.productService.getAll().subscribe(res => {
+      this.products = res;
+    })
+  }
+
+  submit() {
+    this.orderService.update(this.order.id, this.orderForm.value).subscribe(res => {
+      this.toasterService.showSuccess('Orden modificada exitosamente','Operación Exitosa');
+      this.dialogRef.close(res);
+    }, () => {
+      this.toasterService.showError('No se pudo modificar la orden correctamente', 'Error');
+      this.close();
+    });
+  }
+
+  close() {
+    this.dialogRef.close();
   }
 
 }
